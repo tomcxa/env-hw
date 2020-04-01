362ca70570c2436d010c06399771af23179489bc
tomcxa <tomcxa@yandex.ru>
2020-04-01 18:14:38 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
                 test: /\.(png|jpe?g|gif|ico)$/i,
                 loader: 'file-loader',
                 options: {
-                    outputPath: 'images',
+                    outputPath: 'img',
                 },
             },
         ],
